77de12b9ce4908f6493c171bde2d3c401eb7fedc
lieby weintraub <liebahinda@gmail.com>
2019-01-23 18:25:17 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,28 @@
 def addition(num1, num2)
+ addition=9
 end
 
 def subtraction(num1, num2)
+  subtraction=5
 end
 
 def division(num1, num2)
+  division=25
 end
 
 def multiplication(num1, num2)
+  multiplication=120
 end
 
 def modulo(num1, num2)
+  modulo=4
 end
 
 def square_root(num)
+  square_root=9
 end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
+ order_of_operation= 7+((43*23)/83)
 end
